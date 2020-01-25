74109d7cd9b23e46f7e7a55da8e377534f001944
Николай <arcmag-work@mail.ru>
2020-01-26 01:30:42 +0700

    Выполняет шаги из задания - 11. Личный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -9,55 +9,7 @@ const DEFAULT_COUNT = 1;
 const FILE_NAME = `mock.json`;
 const MAX_ANNOUNCE_COUNT = 4;
 
-const TITLES = [
-  `Ёлки.История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок - музыканты 20 - века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево.Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего.Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка.Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно.Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно.Просто действуйте.Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок - музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания.Он обязательно понравится геймерам со стажем.`,
-  `Рок - музыка всегда ассоциировалась с протестами.Так ли это на самом деле ?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать ? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи.Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года.Мощные гитарные рифы и скоростные соло - партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const getDataByFile = async (path) => (await fs.readFile(path)).toString().trim().split(`\n`);
 
 module.exports = {
   name: `--generate`,
@@ -67,15 +19,19 @@ module.exports = {
       process.exit(EXIT_CODE_ERROR);
     }
 
+    const titles = await getDataByFile(`data/titles.txt`);
+    const sentences = await getDataByFile(`data/sentences.txt`);
+    const categories = await getDataByFile(`data/categories.txt`);
+
     const baseDatetime = new Date();
     baseDatetime.setMonth(-3);
 
     const mockData = Array.from({length: +(count || DEFAULT_COUNT)}, () => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
-      announce: shuffle(SENTENCES.slice()).slice(0, getRandomInt(1, MAX_ANNOUNCE_COUNT)),
-      fullText: shuffle(SENTENCES.slice()).slice(0, getRandomInt(1, SENTENCES.length)),
+      title: titles[getRandomInt(0, titles.length - 1)],
+      announce: shuffle(sentences.slice()).slice(0, getRandomInt(1, MAX_ANNOUNCE_COUNT)),
+      fullText: shuffle(sentences.slice()).slice(0, getRandomInt(1, sentences.length)),
       createdDate: new Date(getRandomInt(+baseDatetime, Date.now())).toISOString().replace(/T/, ` `).replace(/\..*$/, ``),
-      category: shuffle(CATEGORIES.slice()).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
+      category: shuffle(categories.slice()).slice(0, getRandomInt(1, categories.length - 1)),
     }));
 
     try {
