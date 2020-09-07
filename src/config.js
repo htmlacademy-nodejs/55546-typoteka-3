bff6c598a345aeaa91f0d7cad85e71e8a5438702
Николай <arcmag-work@mail.ru>
2020-09-07 13:24:36 +0700

    Правки для хероку

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 const dotenv = require(`dotenv`);
 const result = dotenv.config();
 
-module.exports = Object.assign({}, result.error ? {} : result.parsed);
+module.exports = Object.assign({}, result.error ? process.env : result.parsed);
 
 // if (result.error) {
 //   throw result.error;
