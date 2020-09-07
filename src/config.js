aa2e3f251381c6f34468406f3766faf4e645a6b4
Николай <arcmag-work@mail.ru>
2020-09-07 13:45:40 +0700

    Правки

--- src/config.js
+++ src/config.js
@@ -4,9 +4,3 @@ const dotenv = require(`dotenv`);
 const result = dotenv.config();
 
 module.exports = Object.assign({}, result.error ? process.env : result.parsed);
-
-// if (result.error) {
-//   throw result.error;
-// }
-
-// module.exports = Object.assign({}, result.parsed);
